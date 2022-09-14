4c8af9866761ebb264ab1cbe4575c5740dbdf2cb
leximainw <leximainw@gmail.com>
2022-09-14 16:25:01 -0400

    fix: only shuffle answers when question is first created

--- src/TriviaQuestion.js
+++ src/TriviaQuestion.js
@@ -1,18 +1,23 @@
+import {useState} from 'react'
 import {htmlDecode, shuffleArray} from './utils'
 
 const TriviaQuestion = ({question, setCorrect}) => {
-    let allAnswers = [...question.incorrect_answers]
-    allAnswers.push(question.correct_answer)
-    allAnswers = shuffleArray(allAnswers)
+    const [answers, setAnswers] = useState([])
+    if (!answers.length) {
+        let allAnswers = [...question.incorrect_answers]
+        allAnswers.push(question.correct_answer)
+        allAnswers = shuffleArray(allAnswers)
+        setAnswers(allAnswers)
+    }
     return (
         <article>
-            {htmlDecode(question.question)}
-            <div>{allAnswers.map(answer => (
+            <div>{htmlDecode(question.question)}</div>
+            <div className='btn-group'>{answers.map(answer => (
                 <button
                     className='btn btn-primary'
                     key={answer}
                     onClick={() => setCorrect(answer === question.correct_answer)}
-                >{answer}</button>
+                >{htmlDecode(answer)}</button>
             ))}</div>
         </article>
     )
