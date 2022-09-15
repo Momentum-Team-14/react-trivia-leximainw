806493766aa65c382874a6235b203e0a47186926
leximainw <leximainw@gmail.com>
2022-09-15 12:46:40 -0400

    feat: use opentdb API token to avoid repeating questions already seen

--- src/Trivia.js
+++ src/Trivia.js
@@ -13,8 +13,9 @@ const Trivia = () => {
     const [quizState, setQuizState] = useState(0)
     const [currCategory, setCurrCategory] = useState(null)
     const [categoryUpdateFlag, setCatUpFlag] = useState(false)
-    const [questions, setQuestions] = useState([])
+    const [questions, setQuestions] = useState(null)
     const [answers, setAnswers] = useState([])
+    const [apiToken, setApiToken] = useState(null)
 
     useEffect(() => {
         axios.get('https://opentdb.com/api_category.php')
@@ -24,17 +25,22 @@ const Trivia = () => {
             )))
     }, [])
     useEffect(() => {
+        axios.get('https://opentdb.com/api_token.php?command=request')
+            .then(res => setApiToken(res.data.token))
+    }, [])
+    useEffect(() => {
         if (currCategory) {
-            axios.get(`https://opentdb.com/api.php?amount=10&category=${currCategory.id}`)
+            axios.get(`https://opentdb.com/api.php?amount=10&category=${currCategory.id}${apiToken && `&token=${apiToken}`}`)
                 .then(res => {
                     setQuestions(res.data.results)
                 })
         }
-    }, [currCategory, categoryUpdateFlag])
+    }, [currCategory, categoryUpdateFlag, apiToken])
 
     const categoryList = <CategoryList
         categories={categories}
         onSelected={category => {
+            setQuestions(null)
             setCurrCategory(category)
             setQuizState(TAKING_QUIZ)
             setCatUpFlag(!categoryUpdateFlag)
@@ -45,18 +51,37 @@ const Trivia = () => {
         case SELECTING_CATEGORY:
             return categoryList
         case TAKING_QUIZ:
-            return (
-                <div style={{display: 'flex'}}>
-                    {categoryList}
-                    <QuizQuestions
-                        questions={questions}
-                        onComplete={answers => {
-                            setAnswers(answers)
-                            setQuizState(QUIZ_RESULTS)
-                        }}
-                    />
-                </div>
-            )
+            if (!questions) {
+                return (
+                    <div style={{display: 'flex'}}>
+                        {categoryList}
+                        <div>Loading questions, please wait...</div>
+                    </div>
+                )
+            } else if (questions.length) {
+                return (
+                    <div style={{display: 'flex'}}>
+                        {categoryList}
+                        <QuizQuestions
+                            questions={questions}
+                            onComplete={answers => {
+                                setAnswers(answers)
+                                setQuizState(QUIZ_RESULTS)
+                            }}
+                        />
+                    </div>
+                )
+            } else {
+                return (
+                    <div style={{display: 'flex'}}>
+                        {categoryList}
+                        <div>
+                            <p>You've answered all the trivia questions matching your criteria!</p>
+                            <p>Select another category, or click "clear token" to start over.</p>
+                        </div>
+                    </div>
+                )
+            }
         case QUIZ_RESULTS:
             return (
                 <div style={{display: 'flex'}}>
