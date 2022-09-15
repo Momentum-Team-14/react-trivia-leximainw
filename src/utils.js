ade474045b6828c5af6747426f8269dd54f271af
leximainw <leximainw@gmail.com>
2022-09-15 11:53:05 -0400

    style: convert htmlDecode to arrow function

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,6 @@
-function htmlDecode(input) {
-    var doc = new DOMParser().parseFromString(input, "text/html")
-    return doc.documentElement.textContent
-}
+const htmlDecode = input => new DOMParser()
+    .parseFromString(input, "text/html")
+    .documentElement.textContent
 
 const shuffleArray = arr => {
     arr = [...arr]
