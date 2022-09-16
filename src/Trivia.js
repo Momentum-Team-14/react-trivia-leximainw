8dadbc12f44eb59094ebac5afadd3ea7ecbed0e3
leximainw <leximainw@gmail.com>
2022-09-16 08:42:50 -0400

    refactor: simplify Trivia by creating TriviaQuiz component

--- src/Trivia.js
+++ src/Trivia.js
@@ -1,91 +1,39 @@
-import {useEffect, useState} from 'react'
 import axios from 'axios'
-import QuestionResults from './QuestionResults'
-import TriviaQuestions from './TriviaQuestions'
+import {useEffect, useState} from 'react'
 import CategoryList from './CategoryList'
-
-const SELECTING_CATEGORY = 0
-const TAKING_QUIZ = 1
-const QUIZ_RESULTS = 2
+import TriviaQuiz from './TriviaQuiz'
 
 const Trivia = () => {
-    const [quizState, setQuizState] = useState(0)
     const [currCategory, setCurrCategory] = useState(null)
-    const [categoryUpdateFlag, setCatUpFlag] = useState(false)
-    const [questions, setQuestions] = useState(null)
-    const [answers, setAnswers] = useState([])
     const [apiToken, setApiToken] = useState(null)
+    const [resetToggle, setResetToggle] = useState(false)
+    const reset = () => setResetToggle(!resetToggle)
 
     useEffect(() => {
         axios.get('https://opentdb.com/api_token.php?command=request')
             .then(res => setApiToken(res.data.token))
     }, [])
-    useEffect(() => {
-        if (currCategory) {
-            axios.get(`https://opentdb.com/api.php?amount=10&category=${currCategory.id}${apiToken && `&token=${apiToken}`}`)
-                .then(res => {
-                    setQuestions(res.data.results)
-                })
-        }
-    }, [currCategory, categoryUpdateFlag, apiToken])
 
     const categoryList = <CategoryList
         onSelected={category => {
-            setQuestions(null)
             setCurrCategory(category)
-            setQuizState(TAKING_QUIZ)
-            setCatUpFlag(!categoryUpdateFlag)
+            reset()
         }}
     />
 
-    switch (quizState) {
-        case SELECTING_CATEGORY:
-            return categoryList
-        case TAKING_QUIZ:
-            if (!questions) {
-                return (
-                    <div style={{display: 'flex'}}>
-                        {categoryList}
-                        <div>Loading questions, please wait...</div>
-                    </div>
-                )
-            } else if (questions.length) {
-                return (
-                    <div style={{display: 'flex'}}>
-                        {categoryList}
-                        <TriviaQuestions
-                            questions={questions}
-                            onComplete={answers => {
-                                setAnswers(answers)
-                                setQuizState(QUIZ_RESULTS)
-                            }}
-                        />
-                    </div>
-                )
-            } else {
-                return (
-                    <div style={{display: 'flex'}}>
-                        {categoryList}
-                        <div>
-                            <p>You've answered all the trivia questions matching your criteria!</p>
-                            <p>Select another category, or click "clear token" to start over.</p>
-                        </div>
-                    </div>
-                )
-            }
-        case QUIZ_RESULTS:
-            return (
-                <div style={{display: 'flex'}}>
-                    {categoryList}
-                    <QuestionResults answers={answers} questions={questions} />
-                </div>
-            )
-        default:
-            return (
-                <div style={{display: 'flex'}}>
-                    <div>Unexpected quiz state {quizState}</div>
-                </div>
-            )
+    if (!currCategory) {
+        return categoryList
+    } else {
+        return (
+            <div style={{display: 'flex'}}>
+                {categoryList}
+                <TriviaQuiz
+                    category={currCategory}
+                    apiToken={apiToken}
+                    resetToggle={resetToggle}
+                />
+            </div>
+        )
     }
 }
 
---
+++ src/TriviaQuiz.js
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import {useEffect, useState} from 'react'
+import QuestionResults from './QuestionResults'
+import TriviaQuestions from './TriviaQuestions'
+
+const TriviaQuiz = ({category, apiToken, resetToggle, onComplete = _ => {}}) => {
+    const [answers, setAnswers] = useState([])
+    const [questions, setQuestions] = useState(null)
+    const [quizCompleted, setQuizCompleted] = useState(false)
+
+    useEffect(() => {
+        if (category) {
+            axios.get(`https://opentdb.com/api.php?amount=10&category=${category.id}${apiToken && `&token=${apiToken}`}`)
+                .then(res => {
+                    setQuestions(res.data.results)
+                })
+        }
+    }, [category, resetToggle, apiToken])
+
+    if (!quizCompleted) {
+        if (!questions) {
+            return (
+                <div>Loading questions, please wait...</div>
+            )
+        } else if (questions.length) {
+            return (
+                <TriviaQuestions
+                    questions={questions}
+                    onComplete={answers => {
+                        setAnswers(answers)
+                        setQuizCompleted(true)
+                        onComplete(answers)
+                    }}
+                />
+            )
+        } else {
+            return (
+                <div>
+                    <p>You've answered all the trivia questions matching your criteria!</p>
+                    <p>Select another category, or click "clear token" to start over.</p>
+                </div>
+            )
+        }
+    } else {
+        return <QuestionResults answers={answers} questions={questions} />
+    }
+}
+
+export default TriviaQuiz
